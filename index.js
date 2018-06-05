a592d7412fbf6ca0673fdb9fb848919552f8685b
github username <github email address>
2018-06-05 17:58:25 -0400

    Shawn Koski

--- index.js
+++ index.js
@@ -4,18 +4,95 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
 
+    each: function(collection, iteratee) {
+      for (const i in collection) {
+        iteratee(collection[i]);
+      }
+      return collection;
     },
 
-    map: function() {
 
+    map: function(collection, iteratee) {
+      let newArr = [];
+      for (const i in collection) {
+        newArr.push(iteratee(collection[i]));
+      }
+    return newArr;
     },
 
-    reduce: function() {
 
+    reduce: function(collection, iteratee, acc) {
+      let finalAcc = acc;
+      for (const i of collection) {
+        finalAcc = iteratee(finalAcc, i, collection);
+      }
+      return finalAcc;
     },
-    
+
+    find: function(collection, predicate) {
+      for (const i in collection) {
+        if (predicate(collection[i]) === true) {
+          return collection[i];
+        }
+      }
+    },
+
+
+    filter: function(collection, predicate) {
+      const newArr = [];
+      for (const i in collection) {
+        if (predicate(collection[i]) === true) {
+          newArr.push(collection[i]);
+        }
+      }
+      return newArr;
+    },
+
+    size: function(collection) {
+      return Object.keys(collection).length;
+    },
+
+    first: function(array, n) {
+      const newArr = [];
+      if (n === undefined) {
+        return array[0];
+      } else {
+        return array.slice(0, n);
+      }
+    },
+
+    last: function(array, n) {
+      const newArr = [];
+      if (n === undefined) {
+        return array[array.length-1];
+      } else {
+        return array.slice(array.length - n);
+      }
+    },
+
+    compact: function(array) {
+      const newArr = [];
+      for (const i in array){
+        if (array[i]) {
+          newArr.push(array[i]);
+        }
+      }
+      return newArr;
+    },
+
+    sortBy: function(array, iteratee){
+    },
+
+
+    flatten: function(array) {
+      
+    },
+
+
+
+
+
     functions: function() {
 
     },
